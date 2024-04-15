020905d0deb90c7d638753b560291aa5d5d2f411
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-04-15 22:23:14 +0200

    Add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const notion = require("./services/notion");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.get("/database", async (req, res) => {
   const data = await notion.getDatabase();
