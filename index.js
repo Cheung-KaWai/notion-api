e6726159775443e448792886a7ad00b37f6d4a5f
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-04-15 23:47:07 +0200

    Add new routes for page content

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", async (req, res) => {
+  res.send("welcome to my notion-api hehe :))");
+});
+
 app.get("/database", async (req, res) => {
   const data = await notion.getDatabase();
   res.send(data);
+});
+
+app.get("/articles", async (req, res) => {
+  const pages = await notion.getDatabaseContent();
+  const pageContent = await notion.getPageContent(pages[0]);
+  res.send(pageContent);
 });
 
 app.listen(process.env.PORT);
--- services/notion.js
+++ services/notion.js
@@ -2,23 +2,37 @@ const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DATABASE_ID;
+const propDate = process.env.NOTION_DATE_ID;
 
 async function getDatabase() {
   const response = await notion.databases.retrieve({ database_id: process.env.NOTION_DATABASE_ID });
-  console.log("hello");
   return response;
 }
 async function getDatabaseContent() {
-  // const response = await notion.databases.query({
-  //   database_id: databaseId,
-  //   sorts: [
-  //     {
-  //       property: "Last ordered",
-  //       direction: "ascending",
-  //     },
-  //   ],
-  // });
-  console.log("hello");
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    sorts: [
+      {
+        property: propDate,
+        direction: "descending",
+      },
+    ],
+  });
+  const data = response.results.map((page) => page.id);
+  return data;
 }
 
-module.exports = { getDatabase };
+async function getPageContent(pageId) {
+  const props = await notion.pages.retrieve({ page_id: pageId });
+  const content = await notion.blocks.children.list({
+    block_id: pageId,
+    page_size: 100,
+  });
+  const response = {
+    props,
+    content,
+  };
+  return response;
+}
+
+module.exports = { getDatabase, getDatabaseContent, getPageContent };
